cf8433af078a9a06558b72adca7ce3c7722bc32f
albope <albertobort@gmail.com>
2024-08-25 01:47:04 +0200

    workflow and config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,12 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 });
